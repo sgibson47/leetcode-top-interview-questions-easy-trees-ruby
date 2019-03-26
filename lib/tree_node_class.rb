213f506e79ff5292233fb2faf4a2628105d11eb9
sgibson47 <sgibson47@gmail.com>
2019-03-26 15:48:52 -0500

    add file with TreeNode class as prescribed by leetcode

---
+++ lib/tree_node_class.rb
@@ -0,0 +1,8 @@
+# Definition for a binary tree node.
+class TreeNode
+    attr_accessor :val, :left, :right
+    def initialize(val)
+        @val = val
+        @left, @right = nil, nil
+    end
+end
