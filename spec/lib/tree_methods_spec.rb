0a7b02659de89d11d2e27a079d7f679f05619a05
sgibson47 <sgibson47@gmail.com>
2019-03-26 15:58:16 -0500

    add spec for #make_tree

--- spec/lib/tree_methods_spec.rb
+++ spec/lib/tree_methods_spec.rb
@@ -1,3 +1,19 @@
 require 'spec_helper'
 require '/Users/samanthagibson/Development/code/leetcode-top-interview-questions-easy-trees-ruby/lib/tree_methods.rb'
 require '/Users/samanthagibson/Development/code/leetcode-top-interview-questions-easy-trees-ruby/lib/tree_node_class.rb'
+
+describe "#make_tree" do 
+  describe "creates a tree from an array" do
+    array = [3,9,20,null,null,15,7]
+    root = make_tree(array)
+    
+    it "returns the first node in the list" do 
+      expect(root.val).to eq(3)
+    end
+
+    it "first node points to the correct left & right nodes" do 
+      expect(root.left.val).to eq(9)
+      expect(root.right.val).to eq(20)
+    end
+  end
+end
