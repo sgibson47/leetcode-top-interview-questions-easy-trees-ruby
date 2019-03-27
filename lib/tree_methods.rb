253cfb0af562cd1f72a2d9bdd406fe2efbe41eee
sgibson47 <sgibson47@gmail.com>
2019-03-27 15:48:45 -0500

    add #make_tree to generate tree from array

--- lib/tree_methods.rb
+++ lib/tree_methods.rb
@@ -1,1 +1,27 @@
 require 'tree_node_class.rb'
+
+def make_tree(array)
+  #should return the root of the newly created tree
+  # create a TreeNode of array element
+  # store same in a hash at a key of index+1
+  # iterate through hash assigning children to nodes
+  # if parent is at n, left = 2n & right = 2n+1
+  nodes = Hash.new(0)
+  array.each_with_index do |val, i|
+    node = TreeNode.new(val)
+    nodes[i+1] = node
+  end
+
+  nodes.each do |n, node|
+    if node
+      if nodes[2*n]
+        node.left = nodes[2*n]
+      end
+      if nodes[2*n +1]
+        node.right = nodes[2*n+1]
+      end
+    end
+  end
+
+  nodes[1]
+end
